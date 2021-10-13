fb05cba9e83f0a618b680e6c840e560a9d7f0c4c
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-10-13 07:32:53 +0300

    add comments

--- scripts/color-switch.js
+++ scripts/color-switch.js
@@ -7,19 +7,26 @@ const colors = [
   "#795548",
 ];
 
+// ссылки на объекты
 const refs = {
   body: document.querySelector("body"),
   btnStart: document.querySelector("button[data-action='start']"),
   btnStop: document.querySelector("button[data-action='stop']"),
 };
 
+// задать изанчальный цвет
 let currentColor,
   newColor = "#FFFFFF";
 
+// идентификатор таймера изменения цвета
+let timerIdStart;
+
+// сгенерировать случайный индекс из массива цветов
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
+// сменить цвет
 const changeColor = () => {
   do {
     newColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
@@ -29,20 +36,22 @@ const changeColor = () => {
   currentColor = newColor;
 };
 
+// задать статус кнопок Стар и Стоп
 const btnsSatus = (isOn) => {
   refs.btnStart.disabled = isOn;
   refs.btnStop.disabled = !isOn;
 };
 
-let timerIdStart;
-
+// исходное значение кнопок Старт и Стоп
 btnsSatus(false);
 
+// слушать нажатие на Старт
 refs.btnStart.addEventListener("click", () => {
   btnsSatus(true);
   timerIdStart = setInterval(changeColor, 1000);
 });
 
+// слушать нажатие на Сторп
 refs.btnStop.addEventListener("click", () => {
   btnsSatus(false);
   clearInterval(timerIdStart);
